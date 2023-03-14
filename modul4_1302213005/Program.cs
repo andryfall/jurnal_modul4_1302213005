b261c0f84563cf78095015f0079c1d8227aa3a4f
PRAKTIKAN <127047367+bintangrayhan@users.noreply.github.com>
2023-03-14 16:41:57 +0700

    menambah class KodeBuah

--- modul4_1302213005/Program.cs
+++ modul4_1302213005/Program.cs
@@ -1,10 +1,25 @@
 ﻿namespace modul4_1302213005
 {
+    using static KodeBuah;
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            KodeBuah k = new KodeBuah();
+            Buah buah = Buah.Apel;
+            Console.WriteLine(getKodeBuah(buah));
+        }
+    }
+    
+    internal class KodeBuah
+    {
+        public KodeBuah() { }
+        
+        public enum Buah { Apel, Aprikot, Alpukat, Pisang, Paprika, Blackberry, Ceri, Kelapa, Jagung }
+        public static String getKodeBuah(Buah buah)
+        {
+            String[] kodeBuah = { "A00", "B00","C00", "D00", "E00", "F00", "H00", "I00", "J00" };
+            return kodeBuah[(int)buah];
         }
     }
 }
